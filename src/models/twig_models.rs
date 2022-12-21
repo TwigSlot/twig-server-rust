6b441fef2a760ddf09c0ba34d1edfdf1917cc4a0
Robert <xiurobert@gmail.com>
2022-12-21 12:04:41 +0800

    halfway finished whoops

--- src/models/twig_models.rs
+++ src/models/twig_models.rs
@@ -1,5 +1,33 @@
-struct Tag {}
+use crate::models::types::VertexKey;
+use chrono::{DateTime, Utc};
+use uuid::Uuid;
+use palette::Srgb;
+use url::Url;
 
-struct Project {}
+pub struct BaseDbObject {
+    pub id: VertexKey,
+    pub creation_datetime: DateTime<Utc>,
+    pub last_modification_datetime: DateTime<Utc>
+}
 
-struct Resource {}
+pub struct BaseTwigObject {
+    pub db_object: BaseDbObject,
+    pub name: String,
+    pub description: String,
+}
+
+pub struct Tag {
+    pub twig_object: BaseTwigObject,
+    pub color: Srgb
+}
+
+pub struct Project<'a> {
+    pub twig_object: BaseTwigObject,
+    pub owner: Uuid,
+    pub tags: Vec<&'a Tag>
+}
+
+pub struct Resource {
+    pub twig_object: BaseTwigObject,
+    pub url: Url
+}
--- src/models/types.rs
+++ src/models/types.rs
@@ -1,12 +1,19 @@
 use std::collections::{HashMap, HashSet};
 
-type ResourceId = u32;
-type ProjectId = u32;
-type TagId = u32;
+pub enum VertexKey {
+    Tag(u32),
+    Resource(u32),
+    Project(u32),
+}
 
-type VertexKey = u32;
-
+/// A graph (like in discrete mathematics)
+/// where vertices are of type `T`
 struct Graph<T> {
     vertices: HashMap<VertexKey, T>,
     edges: HashMap<VertexKey, HashSet<VertexKey>>,
+}
+
+struct EdgeRelation {
+    from: VertexKey,
+    to: VertexKey,
 }
